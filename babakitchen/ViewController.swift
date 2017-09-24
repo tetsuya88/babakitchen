a8cba509c8cfd053c747bc7d90d2b80258250882
tetsuya88 <yata.gara38@gmail.com>
2017-09-24 23:52:10 +0900

    make project

---
+++ babakitchen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babakitchen
+//
+//  Created by menteadmin on 2017/09/24.
+//  Copyright © 2017年 tetsuya yamaura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
